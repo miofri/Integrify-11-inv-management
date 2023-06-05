9629c4385d74fec4056de61ec2979d51506081ec
Selvi <slvkesuma@gmail.com>
2023-06-05 15:50:03 +0300

    added remove item and add item in inventory

--- Program.cs
+++ Program.cs
@@ -2,11 +2,11 @@
 
 public class Item
 {
-    private int _barcode;
+    private string _barcode;
     private string _name;
     private int _quantity;
 
-    public Item(int barcode, string name, int quantity)
+    public Item(string barcode, string name, int quantity)
     {
         _barcode = barcode;
         _name = name;
@@ -33,9 +33,86 @@ public class Item
         get { return _quantity; }
     }
 
-    public int GetBarcode
+    public string GetBarcode
     {
         get { return _barcode; }
+    }
+}
+
+public class Inventory
+{
+    private List<Item> _items;
+    private int maxCapacity;
+    private int currentInventoryQuantity;
+    private static readonly Inventory instance = new Inventory(100);
+
+    private Inventory(int capacity)
+    {
+        maxCapacity = capacity;
+        currentInventoryQuantity = 0;
+        Console.WriteLine("Capacity: {0}", maxCapacity);
+
+        _items = new List<Item>();
+    }
+
+    public static Inventory Instance
+    {
+        get { return instance; }
+    }
+
+    public bool AddItem(Item item, int quantity)
+    {
+        if (currentInventoryQuantity + quantity > maxCapacity)
+        {
+            Console.WriteLine("Inventory capacity exceeded!");
+            return false;
+        }
+        if (_items.Any(existingItem => existingItem.GetBarcode == item.GetBarcode))
+        {
+            int index = _items.FindIndex(i => i.GetBarcode == item.GetBarcode);
+            currentInventoryQuantity = currentInventoryQuantity - _items[index].GetQuantity;
+            if (index != -1)
+            {
+                _items[index] = item;
+            }
+            Console.WriteLine("Replaced item");
+        }
+        else
+        {
+            _items.Add(item);
+            Console.WriteLine("Added item");
+        }
+        currentInventoryQuantity = currentInventoryQuantity + quantity;
+
+        foreach (var listitem in _items)
+        {
+            Console.WriteLine(
+                $"{listitem.GetItemName} {listitem.GetBarcode} {listitem.GetQuantity}"
+            );
+        }
+        return true;
+    }
+
+    public bool RemoveItem(string barcodeToDelete)
+    {
+        Item itemToRemove;
+        try
+        {
+            itemToRemove = _items.Single(item => item.GetBarcode == barcodeToDelete);
+        }
+        catch (System.InvalidOperationException)
+        {
+            Console.WriteLine("Item not found in inventory!");
+            return false;
+        }
+        _items.Remove(itemToRemove);
+        foreach (var listitem in _items)
+        {
+            Console.WriteLine(
+                $"{listitem.GetItemName} {listitem.GetBarcode} {listitem.GetQuantity}"
+            );
+        }
+        return true;
     }
 }
 
@@ -43,8 +120,16 @@ public class Program
 {
     static void Main()
     {
-        Item NewItem = new Item(3, "Test", 10);
-        NewItem.DecreaseQuantity();
-        Console.WriteLine(NewItem.GetQuantity);
+        Item NewItem2 = new Item("1ABC", "Yksi", 1);
+        Item NewItem3 = new Item("2ABC", "Kaksi", 5);
+        Item NewItem = new Item("3ABC", "Kolme", 10);
+        Item NewItem4 = new Item("4ABC", "Neljä", 4);
+
+        Inventory Inventory = Inventory.Instance;
+        Inventory.AddItem(NewItem, NewItem.GetQuantity);
+        Inventory.AddItem(NewItem2, NewItem2.GetQuantity);
+        Inventory.AddItem(NewItem3, NewItem3.GetQuantity);
+        Inventory.AddItem(NewItem3, NewItem3.GetQuantity);
+        Inventory.RemoveItem("9ABC");
     }
 }
